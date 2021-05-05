92bafb575f4b9a137be7e6be59a05887d11a6afe
Danna Machi <serenita15@gmail.com>
2021-05-05 16:18:54 +1000

    add tier and pit obstacles

--- Items/ObstacleGenerator.gd
+++ Items/ObstacleGenerator.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 export (PackedScene) var Rock
 export (PackedScene) var BigRock
+export (PackedScene) var LevelRock
+export (PackedScene) var PitRock
 
 var sprite_type = "CAVE"
 
@@ -20,12 +22,22 @@ func _on_SpawnTimer_timeout():
 	$SpawnTimer.wait_time = 3 + rand_range(-0.3, 2.3)
 	
 	var rock
-	if randi() % 10 == 0:
+	var num = randi()
+	if num % 3 == 0:
 		rock = BigRock.instance()
 		rock.position = $SpawnPoint2.position
-	else:
+	elif num % 3 == 1:
 		rock = Rock.instance()
 		rock.position = $SpawnPoint.position
+	else:
+		num = randi()
+		if num % 2 == 0:
+			rock = LevelRock.instance()
+			rock.position = $SpawnPoint2.position
+		else:
+			rock = PitRock.instance()
+			rock.position = $SpawnPoint2.position
+			
 		
 	rock.set_sprite_type(sprite_type)
 		
