0dc1351a152d144cc82d15eead2d3b66fb9ca5c3
Danna Machi <serenita15@gmail.com>
2021-05-05 16:19:15 +1000

    fix looping whistle

--- Cutscenes/CutsceneB.gd
+++ Cutscenes/CutsceneB.gd
@@ -4,6 +4,7 @@ signal end(cutsName)
 
 
 var cutscene_name = "N/A"
+var sound_played = false
 
 func setName(cutName):
 	cutscene_name = cutName
@@ -20,6 +21,7 @@ var pic_index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Whistle.stream.loop_mode = 0
 #	$Transitions.show_screen()
 #	yield($Transitions, "transition_in_done")
 	$TextureRect.texture = pic_list[pic_index]
@@ -36,5 +38,7 @@ func _on_Timer_timeout():
 	pic_index += 1
 	if pic_index < len(pic_list):
 		$TextureRect.texture = pic_list[pic_index]
+		if pic_index == 2: $Whistle.play()
+		elif pic_index == 3: $Roar.play()
 	else:
 		emit_signal("end", cutscene_name)
